cb4d62cca56400ff6fead060263e5a91fcc1a06b
Michael Adeshina <michaelking4christ@gmail.com>
2022-10-13 05:41:29 +0100

    implemented DRY to mode.js

--- mode.js
+++ mode.js
@@ -11,34 +11,25 @@ const changeImage = (color) => {
     images[2].setAttribute('src', `assets/img/undraw_version_control_${color}.svg`);
 }
 
-// Dark Mode Style
-const darkMode = () => {
-    switchBox.style.background = 'rgb(150 65 255 / 80%)';
-    changeImage('dark');
+// to check for light or dark mode
+const darkOrLightMode = (isItDark) => {
+    switchBox.style.background = isItDark ? 'rgb(150 65 255 / 80%)' : 'rgb(0 0 0 / 50%)';
+    isItDark ? changeImage('dark') : changeImage('light');
     // changing and customizing the icon to moon
-    switchIcon.classList.replace('fa-sun', 'fa-moon');
-    switchIcon.style.color = '#121212';
-}
-
-// Light Mode Style
-const lightMode = () => {
-    switchBox.style.background = 'rgb(0 0 0 / 50%)';
-    changeImage('light');
-    // changing and customizing the icon to sun
-    switchIcon.classList.replace('fa-moon', 'fa-sun');
-    switchIcon.style.color = '#FF5C5C';
+    isItDark ? switchIcon.classList.replace('fa-sun', 'fa-moon') : switchIcon.classList.replace('fa-moon', 'fa-sun');
+    isItDark ? switchIcon.style.color = '#121212' : switchIcon.style.color = '#FF5C5C';
 }
 
 // The switch event
 const switchTheme = (event) => {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+        darkOrLightMode(true);
         // remember switched state
         localStorage.setItem('theme', 'dark');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        darkOrLightMode(false);
         // remember switched state
         localStorage.setItem('theme', 'light');
     }
@@ -55,6 +46,6 @@ if (currentTheme) {
 
     if (currentTheme === 'dark') {
         switchBtn.checked = true;
-        darkMode();
+        darkOrLightMode(true);
     }
 }
